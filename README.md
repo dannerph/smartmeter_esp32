f884120a8954da06696aadcfd796f76ad30bfa0c
Philipp Danner <philipp@danner-web.de>
2020-09-27 13:23:42 +0200

    add config template

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # smartmeter_esp32
 Smart energy meter (Logarex LK13BE) reading via IR to MQTT
+
+Copy/Rename ```config_template.h``` to ```config.h``` and change relevant parameters.
+
+Build project using plattformiIO
--- src/config_template.h
+++ src/config_template.h
@@ -1,7 +1,7 @@
 const char* clientId = "smartmeter";
 const char* mqtt_topic_load = "smartmeter_load/energy/json";
 const char* mqtt_topic_pv = "smartmeter_pv/energy/json";
-const char* mqtt_server = "192.168.178.3";
-const int mqtt_port = 1883;
+const char* mqtt_server = "IP_ADDRESS";
+const int mqtt_port = 1883; // default MQTT port
 const char* wifi_ssid = "WIFI_SSID";
 const char* wifi_password = "xxxxxxxxxx";
